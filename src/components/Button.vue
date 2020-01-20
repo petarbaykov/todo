d62f5cddac791bf42b427f41c1710f31870b4d59
Petar Baykov <petbaik26@gmail.com>
2020-01-20 21:50:16 +0200

    Card and Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <button class="btn" :class="typeof face !== 'undefined' ? `btn-${face}` : ''">
+        <slot></slot>
+    </button>
+
+</template>
+
+<script>
+export default {
+    props:['face'],
+    created(){
+        console.log(this.face)
+    }
+}
+</script>
