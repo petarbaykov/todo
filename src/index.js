13ecebf50d44a836ea5aa8ec88757f659c7c6489
Petar Baykov <petbaik26@gmail.com>
2020-01-21 22:58:51 +0200

    add vee validate

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+import { ValidationProvider } from 'vee-validate';
+import * as validation from './store/ui/validation'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/src/jquery.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
@@ -11,6 +14,7 @@ import './styles/base.css'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.component('ValidationProvider', ValidationProvider);
 
 new Vue({
   router,
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,6 @@
 // TODO: store is currently unused, but added for future convenience
 import Vuex from 'vuex'
 import Vue from 'vue'
-
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
